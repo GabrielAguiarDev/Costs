2f4a589a3bbf41125453bcb19974183c86ec0345
Gabriel Aguiar <gabriel.aguiar.dev@gmail.com>
2022-08-05 20:11:37 -0300

    page contact and company add

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,6 +1,12 @@
+import styles from './Company.module.css'
+
 function Company() {
     return (
-        <h1>Empresa</h1>
+        <div className={styles.container}>
+            <h1>Empresa <span>Costs</span></h1>
+            <h4>Sobre a empresa</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, inventore repellendus illo deserunt quis odit modi ea harum et quisquam laborum maiores consectetur quo laboriosam perspiciatis vitae, blanditiis dolore? Architecto! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, consequuntur. Autem placeat veritatis reiciendis repellat nulla provident, dolorem sapiente voluptatem animi enim nesciunt labore earum atque perferendis vitae recusandae quisquam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic eligendi velit blanditiis omnis quod dolores laudantium saepe accusamus error ea? Maiores, doloremque natus? Fugiat itaque velit voluptatibus, ad commodi ullam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quasi minima nisi, eos possimus magnam. Voluptas sunt repellendus quia, dolores doloremque temporibus totam nisi placeat odit voluptatum voluptates veniam fugit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quam! Sit neque reiciendis earum eos natus quasi qui, porro, unde magnam, in reprehenderit nam quod itaque iste maxime magni cum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis iste explicabo repellat inventore ad corrupti maxime ea, amet vero excepturi soluta, reprehenderit unde aliquam ipsum, voluptatem placeat aliquid dicta nemo.</p>
+        </div>
     )
 }
 
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,23 @@
+import { FaLinkedin, FaWhatsapp, FaGithub } from 'react-icons/fa'
+import styles from "./Contact.module.css";
+
 function Contact() {
-    return (
-        <h1>Contato</h1>
-    )
+  return (
+    <div className={styles.container}>
+      <h1>Contato</h1>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/gabriel-aguiar-dev/"><span>Linkedin</span><FaLinkedin /></a>
+        </li>
+        <li>
+          <a href="https://github.com/GabrielAguiarDev/Costs"><span>Github</span><FaGithub /></a>
+        </li>
+        <li>
+          <a href="https://wa.me/+5573998486884"><span>Whatsapp</span><FaWhatsapp /></a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Contact;
